795b43fcc55e0d2d092f7b3bc4a8ac21db2ee831
jcasadiego <jcasadiego.a@gmail.com>
2022-08-22 06:34:04 -0400

    fix app

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,37 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Layout from '../containers/Layout';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import PasswordRecovery from '../pages/PasswordRecovery';
-import SendEmail from '../pages/SendEmail';
-import NewPassword from '../pages/NewPassword';
-import MyAccount from '../pages/MyAccount';
-import CreateAccount from '../pages/CreateAccount';
-import Checkout from '../pages/Checkout';
-import Orders from '../pages/Orders';
-import NotFound from '../pages/NotFound';
-import '../styles/global.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import PasswordRecovery from "../pages/PasswordRecovery";
+import SendEmail from "../pages/SendEmail";
+import NewPassword from "../pages/NewPassword";
+import MyAccount from "../pages/MyAccount";
+import CreateAccount from "../pages/CreateAccount";
+import Checkout from "../pages/Checkout";
+import Orders from "../pages/Orders";
+import NotFound from "../pages/NotFound";
+import "../styles/global.css";
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Layout>
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/password-recovery" component={PasswordRecovery} />
-					<Route exact path="/send-email" component={SendEmail} />
-					<Route exact path="/new-password" component={NewPassword} />
-					<Route exact path="/account" component={MyAccount} />
-					<Route exact path="/signup" component={CreateAccount} />
-					<Route exact path="/checkout" component={Checkout} />
-					<Route exact path="/orders" component={Orders} />
-					<Route path="*" component={NotFound} />
-				</Switch>
-			</Layout>
-		</BrowserRouter>
+		<Layout>
+			<Routes>
+				<Route path="/" element={Home} />
+				<Route path="/login" element={Login} />
+				<Route path="/password-recovery" element={PasswordRecovery} />
+				<Route path="/send-email" element={SendEmail} />
+				<Route path="/new-password" element={NewPassword} />
+				<Route path="/account" element={MyAccount} />
+				<Route path="/signup" element={CreateAccount} />
+				<Route path="/checkout" element={Checkout} />
+				<Route path="/orders" element={Orders} />
+				<Route path="*" element={NotFound} />
+			</Routes>
+		</Layout>
 	);
-}
+};
 
 export default App;
