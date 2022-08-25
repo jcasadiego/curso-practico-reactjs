d771005e7e9fe31bb6a783f34ddeed3c45824b84
jcasadiego <jcasadiego.a@gmail.com>
2022-08-25 06:25:05 -0400

    fixed add alias

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import '../styles/ProductInfo.scss';
+import React from 'react';
+import addCart from '@icons/bt_add_to_cart.svg';
+import '@styles/ProductInfo.scss';
+
+const bike = 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
 
 const ProductInfo = () => {
     return(
         <>
-            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+            <img src={bike} alt="bike" />
             <div classname="ProductInfo">
                 <p>$35,00</p>
                 <p>Bike</p>
                 <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
                 <button classname="primary-button add-to-cart-button">
-                    <img src="./icons/bt_add_to_cart.svg" alt="add to cart" />
+                    <img src={addCart} alt="add to cart" />
                     Add to cart
                 </button>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ReactDOM  from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './routes/App';
+import App from '@routes/App';
 
 const root = ReactDOM.createRoot(
     document.getElementById("root")
