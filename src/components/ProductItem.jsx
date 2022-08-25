371bbf6bc304473288c2168b61cece63de5e9868
jcasadiego <jcasadiego.a@gmail.com>
2022-08-25 07:24:14 -0400

    used useState

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '@icons/bt_add_to_cart.svg';
 import '@styles/ProductItem.scss';
 
 const bike = 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
 
 const ProductItem = () => {
+
+    const [cart, setCart] = useState([]);
+
+    const handleClick = () => {
+        setCart([]);
+    }
+
     return(
         <div className="ProductItem">
             <img src={bike} alt="" />
@@ -13,7 +20,7 @@ const ProductItem = () => {
                     <p>$120,00</p>
                     <p>Bike</p>
                 </div>
-                <figure>
+                <figure onClick={handleClick}>
                     <img src={logo} alt="" />
                 </figure>
             </div>
